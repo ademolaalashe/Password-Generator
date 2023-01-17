94e4ee480ac11b25d3b11d31aafb047ab89eea6a
ademolaalashe <77229886+ademolaalashe@users.noreply.github.com>
2023-01-17 21:20:19 +0000

    Check and edit overall code

--- script.js
+++ script.js
@@ -100,11 +100,11 @@ let passwordOptions;
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  passwordLength = prompt('Please enter the desired password lenght (minimum of 10 characters and a maximum of 64 characters)')
-}
+  passwordLength = prompt(
+    'Please enter the desired password lenght (minimum of 10 characters and a maximum of 64 characters)'
+  );
 
-
-  // This is to check the input is within the range and is a number
+  // check the input is within the range and is a number
   while (passwordLength < 10 || passwordLength > 64 || isNaN(passwordLength)) {
     alert(
       'Your password length must be within range of 10 to 64 characters. Please try again.'
@@ -126,9 +126,7 @@ function getPasswordOptions() {
     `Include uppercased characters (A-Z) in the password?`
   );
 
-
-
-  // This creates an object with corresponding arrays of characters for each prompt option
+  // This creates object with corresponding arrays of characters for each prompt option
   passwordOptions = [
     { option: includeSpecialCharacters, array: specialCharacters },
     { option: includeNumericCharacters, array: numericCharacters },
